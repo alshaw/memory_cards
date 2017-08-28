ace3d30cd1afc101eb8269bc5810705aa6f0857c
Allison Shaw <shaw.allison91@gmail.com>
2017-08-28 15:00:09 -0600

    not working, will come back to fix

---
+++ puzzle.js
@@ -0,0 +1,48 @@
+var MemoryGame = function() {
+  this.cards = [
+  		{ name: "       ",         img: "aquaman.jpg" },
+  		{ name: "      ",          img: "batman.jpg" },
+  		{ name: "               ", img: "captain-america.jpg" },
+  		{ name: "              ",  img: "fantastic-four.jpg" },
+  		{ name: "     ",           img: "flash.jpg" },
+      { name: "           ",     img: "green-arrow.jpg" },
+  		{ name: "             ",   img: "green-lantern.jpg" },
+  		{ name: "ironman",         img: "ironman.jpg" },
+  		{ name: "spiderman",       img: "spiderman.jpg" },
+  		{ name: "superman",        img: "superman.jpg" },
+  		{ name: "the avengers",    img: "the-avengers.jpg" },
+  		{ name: "thor",            img: "thor.jpg" },
+  		
+  	];
+    this.selectedCards = [];
+    this.pairsClicked = 0;
+    this.correctPairs = 0;
+};
+
+// //******************************************************************
+// // HTML/CSS Interactions
+// //******************************************************************
+
+var memoryGame;
+
+$(document).ready(function(){
+  memoryGame = new MemoryGame();
+  var html = '';
+
+  memoryGame.cards.forEach(function(pic, index) {
+    var sanitizedName = pic.name.split(' ').join('_');
+
+   html += '<div class= "card" name="card_' + sanitizedName + '">';
+    html += '<div class="back"';
+    html += '    name="' + pic.name + '">';
+    html += '</div>';
+    html += '<div class="front" ';
+    html += 'style="background: url(img/' + pic.img + '") no-repeat"';
+    html += '    name="'       + pic.name +  '">';
+    html += '</div>';
+    html += '</div>';
+  });
+
+  // Add all the divs to the HTML
+  document.getElementById('memory_board').innerHTML = html;
+});
